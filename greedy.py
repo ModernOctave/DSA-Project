e63e7cdeebd1b783dd0593d70f5e5a56b152bd13
Om Patil <200010036@iitdh.ac.in>
2021-10-27 12:45:11 +0530

    New python version

---
+++ greedy.py
@@ -0,0 +1,12 @@
+class Tree:
+	def __init__(x, y, height, thickness, unit_weight, unit_value):
+		self.x = x
+		self.y = y
+		self.height = height
+		self.thickness = thickness
+		self.unit_weight = unit_weight
+		self.unit_value = unit_value
+
+time_limit, grid_size, num_trees = input().split(' ')
+
+print(time_limit)
